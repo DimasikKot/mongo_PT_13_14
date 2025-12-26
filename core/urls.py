7db51e2a7f9fa903bc458782298f42b14130bc6f
Кожухарев Дмитрий <dima.kozhuharew@yandex.ru>
2025-12-27 04:11:43 +0500

    feat: половина работает

--- core/urls.py
+++ core/urls.py
@@ -1,19 +1,19 @@
 from django.urls import path
 from . import views
-from . import reports
-
 
 urlpatterns = [
-    path("pizzeria/add/", views.add_pizzeria),
-    path("cookbook/add/", views.add_cookbook),
-    path("pizzerias/", views.get_pizzerias),
-    path("cookbooks/", views.get_cookbooks),
-    path("pizzeria/update/<str:id>/", views.update_pizzeria),
-    path("cookbook/update/<str:id>/", views.update_cookbook),
-    path("pizzeria/delete/<str:id>/", views.delete_pizzeria),
-    path("cookbook/delete/<str:id>/", views.delete_cookbook),
-    path("pizzeria/report/cheap/<int:price>/", reports.cheap_pizzas_report),
-    path("pizzeria/report/expensive/<int:price>/", reports.expensive_pizzas_report),
-    path("cookbook/report/fast/<int:time>/", reports.fast_recipes_report),
-    path("cookbook/report/low/<int:ingredients>/", reports.low_ingredients_report),
+    path("", views.index),
+
+    path("pizzerias/", views.pizzerias_page),
+    path("cookbooks/", views.cookbooks_page),
+
+    path("pizzeria/add/", views.add_pizzeria_page),
+    path("pizzeria/update/<str:id>/", views.update_pizzeria_page),
+    path("pizzeria/delete/<str:id>/", views.delete_pizzeria_page),
+
+    path("cookbook/add/", views.add_cookbook_page),
+    path("cookbook/update/<str:id>/", views.update_cookbook_page),
+    path("cookbook/delete/<str:id>/", views.delete_cookbook_page),
+
+    path("reports/", views.reports_page),
 ]
--- core/views.py
+++ core/views.py
@@ -6,6 +6,119 @@ from bson import ObjectId
 
 import json
 
+from .reports import *
+
+
+def reports_page(request):
+    result = None
+
+    if "type" in request.GET:
+        t = request.GET["type"]
+        v = int(request.GET["value"])
+
+        if t == "cheap":
+            result = list(pizzerias.aggregate(cheap_pipeline(v)))
+        elif t == "expensive":
+            result = list(pizzerias.aggregate(expensive_pipeline(v)))
+        elif t == "fast":
+            result = list(cookbooks.aggregate(fast_pipeline(v)))
+        elif t == "low":
+            result = list(cookbooks.aggregate(low_ingredients_pipeline(v)))
+
+    return render(request, "reports.html", {"result": result})
+
+
+def index(request):
+    return render(request, "index.html")
+
+
+def pizzerias_page(request):
+    data = list(pizzerias.find())
+    return render(request, "pizzerias.html", {"pizzerias": data})
+
+def cookbooks_page(request):
+    data = list(cookbooks.find())
+    return render(request, "cookbooks.html", {"cookbooks": data})
+
+
+def add_pizzeria_page(request):
+    if request.method == "POST":
+        pizzerias.insert_one({
+            "name": request.POST["name"],
+            "address": request.POST["address"],
+            "phone": request.POST["phone"],
+            "pizzas": []
+        })
+        return redirect("/pizzerias/")
+    return render(request, "pizzeria_form.html", {"title": "Добавить пиццерию"})
+
+def update_pizzeria_page(request, id):
+    pizzeria = pizzerias.find_one({"_id": ObjectId(id)})
+
+    if request.method == "POST":
+        pizzerias.update_one(
+            {"_id": ObjectId(id)},
+            {"$set": {
+                "name": request.POST["name"],
+                "address": request.POST["address"],
+                "phone": request.POST["phone"]
+            }}
+        )
+        return redirect("/pizzerias/")
+
+    return render(
+        request,
+        "pizzeria_form.html",
+        {
+            "title": "Редактировать пиццерию",
+            "pizzeria": pizzeria
+        }
+    )
+
+def delete_pizzeria_page(request, id):
+    pizzerias.delete_one({"_id": ObjectId(id)})
+    return redirect("/pizzerias/")
+
+
+def add_cookbook_page(request):
+    if request.method == "POST":
+        cookbooks.insert_one({
+            "cookbook_name": request.POST["cookbook_name"],
+            "recipes": []
+        })
+        return redirect("/cookbooks/")
+
+    return render(
+        request,
+        "cookbook_form.html",
+        {"title": "Добавить кулинарную книгу"}
+    )
+
+def update_cookbook_page(request, id):
+    cookbook = cookbooks.find_one({"_id": ObjectId(id)})
+
+    if request.method == "POST":
+        cookbooks.update_one(
+            {"_id": ObjectId(id)},
+            {"$set": {
+                "cookbook_name": request.POST["cookbook_name"]
+            }}
+        )
+        return redirect("/cookbooks/")
+
+    return render(
+        request,
+        "cookbook_form.html",
+        {
+            "title": "Редактировать кулинарную книгу",
+            "cookbook": cookbook
+        }
+    )
+
+def delete_cookbook_page(request, id):
+    cookbooks.delete_one({"_id": ObjectId(id)})
+    return redirect("/cookbooks/")
+
 
 def add_pizzeria(request):
     if request.method == "POST":
